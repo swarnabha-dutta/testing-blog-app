b4dc8c61796a3985299ebd39d125afe48589dedd
swarnabha <swarnabhadutta909@gmail.com>
2026-06-30 14:12:17 +0530

    understanding how the getByAllByRole of RTL works when we have same type of elements and their value also

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='App'>
-
-      {/* Semantic Elements */}
-      <h1>Multiple Item with Role</h1>
-      <h1>Custom Role</h1>
-      <button>Click 1</button>
-      <button>Click 2 </button>
-      <label htmlFor='input1'>User Input</label>
-      <input id='input1' />
-      <label htmlFor='input2'>User page</label>
-      <input type='text' id='input2' />
-
-
-
-      {/* Non-Semantic Elements */}
-      <div role='dummy'>
-        dummy Text
-      </div>
+      <h1>RTL Query:getAllByRole</h1>
+      <button>Click Me</button>
+      <button>Click Me</button>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-test("btn testing by using getByRole", () => {
+
+
+test("getAllByRole", () => {
   render(<App />);
-  const btn1 = screen.getByRole('button', { name: "Click 1" });
-  const btn2 = screen.getByRole('button', { name: "Click 2" });
-  const inputField1 = screen.getByRole("textbox", { name: /User Input/ });
-  const inputField2 = screen.getByRole("textbox", { name: /User page/ });
-  const dv1 = screen.getByRole('dummy');
-  expect(dv1).toBeInTheDocument()
-
-
-
-
-  expect(btn1).toBeInTheDocument();
-  expect(btn2).toBeInTheDocument();
-  expect(inputField1).toBeInTheDocument();
-  expect(inputField2).toBeInTheDocument();
+  const btns = screen.getAllByRole('button');
+  for (let btn of btns) {
+    expect(btn).toBeInTheDocument();
+  }
 })
