a84b86d0c58421f4ef3ba016166241f99821c7e1
swarnabha <swarnabhadutta909@gmail.com>
2026-06-30 13:37:16 +0530

    understanding how testing in Functional Component in depth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import './App.css'
-
 
 const App = () => {
   return (
-    <div>
-      <h1>getByRole</h1>
-      {/* <input type='text' defaultValue={'hello'} /> // enable now  */}
-      <input type='text' defaultValue={'hello'} disabled /> //disable now
+    <div className='App'>
+
+      {/* Semantic Elements */}
+      <h1>Multiple Item with Role</h1>
+      <h1>Custom Role</h1>
+      <button>Click 1</button>
+      <button>Click 2 </button>
+      <label htmlFor='input1'>User Input</label>
+      <input id='input1' />
+      <label htmlFor='input2'>User page</label>
+      <input type='text' id='input2' />
+
+
+
+      {/* Non-Semantic Elements */}
+      <div role='dummy'>
+        dummy Text
+      </div>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,19 @@
-import { render, screen } from "@testing-library/react"
-import App from "./App"
-
-test("get By Role", () => {
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+test("btn testing by using getByRole", () => {
   render(<App />);
-  const inputField = screen.getByRole("textbox");
-  expect(inputField).toBeInTheDocument();
-  expect(inputField).toHaveValue('hello');
-  expect(inputField).toBeDisabled();
-});
+  const btn1 = screen.getByRole('button', { name: "Click 1" });
+  const btn2 = screen.getByRole('button', { name: "Click 2" });
+  const inputField1 = screen.getByRole("textbox", { name: /User Input/ });
+  const inputField2 = screen.getByRole("textbox", { name: /User page/ });
+  const dv1 = screen.getByRole('dummy');
+  expect(dv1).toBeInTheDocument()
+
+
+
+
+  expect(btn1).toBeInTheDocument();
+  expect(btn2).toBeInTheDocument();
+  expect(inputField1).toBeInTheDocument();
+  expect(inputField2).toBeInTheDocument();
+})
